9d6d7aaf70ac9f7d1d20c3f474869a4afcb821ad
Sahana <sahat@celominds.in>
2026-07-12 10:30:19 +0530

    Refactor Amazon page object by removing unused methods and simplifying search results validation

--- tests/script/amazon.spec.js
+++ tests/script/amazon.spec.js
@@ -4,12 +4,9 @@ const {Amazon} = require('../pages/amazon');
 test("Search a product in amazon", async({page})=>{
     console.log("Test started");
     const amazon = new Amazon(page);
-    const searchTerm = "samsung s25";
     await amazon.goto();
-    await amazon.search(searchTerm);
-    await amazon.searchResults(searchTerm);
-    await amazon.validateFirstProduct(searchTerm);
-    await amazon.addFirstProductToCart();
+    await amazon.search("samsung s25");
+    await amazon.searchResults();
 
     console.log("Test completed successfully");
 });
