a81d776f01d479c438ceeed0feeda595a02f66a8
Sahana <sahat@celominds.in>
2026-07-12 10:59:55 +0530

    Fix missing await for searchResultsCount in product search test

--- tests/script/amazon.spec.js
+++ tests/script/amazon.spec.js
@@ -7,7 +7,7 @@ test("Search a product in amazon", async({page})=>{
     await amazon.goto();
     await amazon.search("samsung s25");
     await amazon.searchResults();
-    
+    await amazon.searchResultsCount()
 
     console.log("Test completed successfully");
 });
